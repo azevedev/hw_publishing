f481facb5b50742c298e282032bb63a0a8668331
Maranhão Azevedo <azevedev@gmail.com>
2025-09-08 01:37:50 -0300

    chore: Commit Savepoint!

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -14,7 +14,6 @@ const getBaseURL = () => {
 const api = axios.create({
   baseURL: getBaseURL(),
   timeout: 10000,
-  withCredentials: true // Include credentials if needed
 });
 
 export default api;
